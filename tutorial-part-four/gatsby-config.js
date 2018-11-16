349b81d218a47336e175d53af323fc0f84e52717
Slava Bezgachev <bezgachev@gmail.com>
2018-11-16 13:52:10 +0600

    Lesson 6 done

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -3,19 +3,25 @@ module.exports = {
     title: "Title from metadata, yo"
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`
       }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
     }
   ]
+};
+
+exports.onCreateNode = ({ node }) => {
+  console.log("Created a new Node...");
+  console.log(node.internal.type);
 };
